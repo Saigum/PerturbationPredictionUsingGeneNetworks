8ca3ada5319c154e0586869a4cf112d17e7b5058
Saigum <113506138+Saigum@users.noreply.github.com>
2025-07-08 16:01:22 +0530

    Update quicksetup.sh

--- quicksetup.sh
+++ quicksetup.sh
@@ -1,6 +1,7 @@
 git clone  https://github.com/Saigum/PerturbationPredictionUsingGeneNetworks.git
 cd PerturbationPredictionUsingGeneNetworks
-uv venv perturb 
+uv venv perturb
+source perturb/bin/activate
 uv pip install --no-cache-dir torch torch_geometric scanpy cell-gears ipykernel
 scp ada.iiit.ac.in/share1/saigum/vcc_data.zip ./
 unzip vcc_data.zip
